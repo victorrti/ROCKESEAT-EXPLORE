99aee33cc63f37b992fe00e9bca7b421cac01bb4
Victor Pereira <victorrodriguesti@gmail.com>
2023-08-26 11:35:18 -0300

    regra que alternar entre as telas quando acerta o numero

--- JOGOADIVINHA/script.js
+++ JOGOADIVINHA/script.js
@@ -1,6 +1,14 @@
-function handleClick(event){
-    event.preventDefault();
-    const inputNumero = document.querySelector("#inputNumero")
-    console.log(inputNumero.value);
-
+const ramdomNumero = Math.round(Math.random() * 10);
+let numeroTentativas = 0;
+function handleClick(event){   
+    event.preventDefault(); 
+    numeroTentativas++; 
+    let inputNumero = document.querySelector("#inputNumero")
+    if(Number(inputNumero.value) == ramdomNumero){
+        document.querySelector(".screean1").classList.add("hide");
+        document.querySelector(".screen2").classList.remove("hide");
+    }
+   
+   
 }
+
