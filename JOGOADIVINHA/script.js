ca133f81691dc2614429a5071eeb7bbdd56ab879
Victor Pereira <victorrodriguesti@gmail.com>
2023-08-26 13:03:55 -0300

    incluir reset apos acerta o numero e clicar em tentar novamente

--- JOGOADIVINHA/script.js
+++ JOGOADIVINHA/script.js
@@ -1,14 +1,31 @@
 const ramdomNumero = Math.round(Math.random() * 10);
-let numeroTentativas = 0;
-function handleClick(event){   
+let numeroTentativas = 1;
+const screen1 = document.querySelector(".screean1");
+const screen2 = document.querySelector(".screen2");
+function handleTryClick(event){   
     event.preventDefault(); 
-    numeroTentativas++; 
+   
     let inputNumero = document.querySelector("#inputNumero")
     if(Number(inputNumero.value) == ramdomNumero){
-        document.querySelector(".screean1").classList.add("hide");
-        document.querySelector(".screen2").classList.remove("hide");
-    }
+        toggleScrean()
+        document.querySelector(".screen2 h2").innerText = `Você acertou em ${numeroTentativas} tentativas`
+    }   
+    numeroTentativas++; 
    
-   
+}
+
+const btnTry = document.querySelector("#try");
+const btnReset = document.querySelector("#btnReset"); 
+btnTry.addEventListener('click',handleTryClick)
+btnReset.addEventListener('click',function(){
+        toggleScrean()
+        document.querySelector('input').value ="";
+        numeroTentativas =1;
+       
+})
+
+function toggleScrean(){
+    screen2.classList.toggle("hide");
+    screen1.classList.toggle("hide");
 }
 
